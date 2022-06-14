a229c994edb300f1ddb81fadbe902a575de07a99
Jack Pena <penajackc@gmail.com>
2022-06-14 10:19:10 -0700

    cleaned files structure and added landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from './Components/Landing';
+
 
 function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Welcome to Jack's Tennis Page
     </h1>
+    <Landing />
+    </>
   );
 }
 
